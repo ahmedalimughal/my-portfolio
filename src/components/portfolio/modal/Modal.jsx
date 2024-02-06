66e23878a2f6e22c519ebc38dc91579f098a1c17
Ahmed Ali Mughal <144814693+ahmedalimughal@users.noreply.github.com>
2024-02-06 10:05:23 -0800

    a

--- src/components/portfolio/modal/Modal.jsx
+++ src/components/portfolio/modal/Modal.jsx
@@ -5,14 +5,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Modal = ({ modalId, setGetModal }) => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: true,
-  };
   if (modalId === 1) {
     return (
       <div className="modal_portfolio ">
