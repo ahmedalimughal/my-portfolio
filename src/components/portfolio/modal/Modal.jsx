40f78ded98f8b8f51fbd46924421f8f5f06a384e
Ahmed Ali Mughal <144814693+ahmedalimughal@users.noreply.github.com>
2024-02-06 10:03:56 -0800

    a

--- src/components/portfolio/modal/Modal.jsx
+++ src/components/portfolio/modal/Modal.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import img1 from "../../../assets/img/portfolio/project-1.jpg";
-import img2 from "../../../assets/img/portfolio/project-2.jpg";
-import portfolioVideo from "../../../assets/img/portfolio/video.mp4";
 import CloseImg from "../../../assets/img/cancel.svg";
 import PortfolioData from "../portfolioData";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
