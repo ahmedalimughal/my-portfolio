ab643c88e2b1d3f7ad4bacfaf5bf6d1cf7eb14db
Hboxdeveloper <thezabihahcrew@gmail.com>
2026-02-01 05:49:42 +0500

    Update Email

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -12,7 +12,7 @@ const Address = () => {
       <p className="open-sans-font custom-span-contact position-relative">
         <i className="fa fa-envelope-open position-absolute"></i>
         <span className="d-block">mail me</span>{" "}
-        <a href="mailto:ahmedalimughal.dev@gmail.com">ahmedalimughal.dev@gmail.com</a>
+        <a href="mailto:ahmedali110@gmail.com">ahmedali110@gmail.com</a>
       </p>
       {/* End .custom-span-contact */}
 
