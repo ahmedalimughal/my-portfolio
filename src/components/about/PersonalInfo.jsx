7d5ed8f3725a34bcd6cdacbe207d53d184371c81
Ahmed Ali Mughal <144814693+ahmedalimughal@users.noreply.github.com>
2026-07-30 06:41:32 +0500

    Fix formatting and clean up PersonalInfo component

--- src/components/about/PersonalInfo.jsx
+++ src/components/about/PersonalInfo.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const personalInfoContent = [
   { meta: "fa fa-user", metaInfo: "Ahmed Ali Mughal" },
@@ -8,14 +7,13 @@ const personalInfoContent = [
   { meta: "fa fa-tasks", metaInfo: "Available" },
   { meta: "fa fa-map-marker", metaInfo: "Karachi, Pakistan" },
   
-  { meta: "fa fa-phone", metaInfo: "+92 330 2529 311"  , metaLink: "tel:+923302529311"},
+  { meta: "fa fa-phone", metaInfo: "+92 330 2529 311", metaLink: "tel:+923302529311" },
   { meta: "fa fa-language", metaInfo: "German, English" },
   
-  { meta: "fa fa-envelope", metaInfo: "ahmedali110@gmail.com" , metaLink: "mailto:ahmedali110@gmail.com"},
-  { meta: "fa fa-linkedin" , metaInfo:"Ahmed Ali Mughal", metaLink: "https://www.linkedin.com/in/ahmed-ali-mughal/"},
+  { meta: "fa fa-envelope", metaInfo: "ahmedali110@gmail.com", metaLink: "mailto:ahmedali110@gmail.com" },
+  { meta: "fa fa-linkedin", metaInfo: "Ahmed Ali Mughal", metaLink: "https://www.linkedin.com/in/ahmed-ali-mughal/" },
 
-  { meta: "fa fa-heart-o", metaInfo:"@ahmedalimughal", metaLink: "https://lovable.dev/@ahmedalimughal"}
-  
+  { meta: "fa fa-heart-o", metaInfo: "@ahmedalimughal", metaLink: "https://lovable.dev/@ahmedalimughal" }
 ];
 
 const PersonalInfo = () => {
@@ -23,12 +21,26 @@ const PersonalInfo = () => {
     <ul className="about-list list-unstyled open-sans-font">
       {personalInfoContent.map((val, i) => (
         <li key={i}>
-          <span className="title px-1 icon"><i className={val.meta}></i></span>
-          <Link to={val.metaLink} className="text-decoration-none text-white">
-            <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
-            {val.metaInfo}            
+          <span className="title px-1 icon">
+            <i className={val.meta}></i>
           </span>
-          </Link>
+
+          {val.metaLink ? (
+            <a
+              href={val.metaLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none text-white"
+            >
+              <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+                {val.metaInfo}
+              </span>
+            </a>
+          ) : (
+            <span className="value d-block d-sm-inline-block d-lg-block d-xl-inline-block">
+              {val.metaInfo}
+            </span>
+          )}
         </li>
       ))}
     </ul>
